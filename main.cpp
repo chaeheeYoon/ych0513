f312042c2349f8d979e96b5ed6465669a0e0c05c
ych <korea940414@naver.com>
2024-05-13 09:43:16 +0900

    사칙연산 삽입

--- main.cpp
+++ main.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 int main()
 {
+	int Number1;
+	int Number2;
 
+
+	Number1 = 0;
+	Number2 = 0;
+
+	cin >> Number1;
+	cin >> Number2;
+
+	cout << Number1 + Number2 << endl;
+	cout << Number1 - Number2 << endl;
+	cout << Number1 / Number2 << endl;
+	cout << Number1 * Number2 << endl;
 
 
 	return 0;
